61f126564a283c5c693009376119ee3bcce116b2
Jay Rutherford <badhairpadre@gmail.com>
2019-01-05 20:16:34 -0700

    fixed MOM to be proper

--- scripts/messages.js
+++ scripts/messages.js
@@ -2,4 +2,4 @@ firstMessage = "Hi sweetie!<br><br>I logged into your MyStudentHub account and s
 
 secondMessage = "Hi again sweetie!<br><br>I see you're still working hard at school. Here's a bit more: $10! Hopefully all this money doesn't make you complacent ;)<br><br>XOXO,<br>Mom"
 
-thirdMessage = "Here's another $20, sweetie! I wish I could send more but Dad thinks you should try to work for a really good GPA... I suggested -500,000. Keep up the good work, and always remember that I'm proud of you!<br><br>XOXO,<br>MOM"
+thirdMessage = "Here's another $20, sweetie! I wish I could send more but Dad thinks you should try to work for a really good GPA... I suggested -500,000. Keep up the good work, and always remember that I'm proud of you!<br><br>XOXO,<br>Mom"
